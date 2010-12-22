2898659d3250217093c36447ce280e6de6bcfd9f
Scott Prager <hakusa@gmail.com>
2010-12-22 14:06:28 -0500

    More tweaking.

--- main.cpp
+++ main.cpp
@@ -886,9 +886,9 @@ int main( int argc, char** argv )
             {
                 Vector<float,2> r = cActors[j]->s - particles[i]->s;
                 if( magnitude(r) > cActors[j]->radius() )
-                    particles[i]->a += magnitude( r, cActors[j]->mass() * (1.0f/100.0f) / std::pow(magnitude(r),1.05f) ) * Arena::scale;
+                    particles[i]->a += magnitude( r, cActors[j]->mass() * (1.0f/50.0f) / std::pow(magnitude(r),1.3f) ) * Arena::scale;
                 else
-                    particles[i]->a += magnitude( r, cActors[j]->mass() * (1.0f/1000.0f) / std::pow(magnitude(r),3.25f) ) * Arena::scale;
+                    particles[i]->a += magnitude( r, cActors[j]->mass() * (1.0f/10000.0f) / std::pow(magnitude(r),2.f) ) * Arena::scale;
             }
         };
 
